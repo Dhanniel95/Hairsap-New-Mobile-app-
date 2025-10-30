3dfce21975fd1feedc6203e1bb24a8fb915ecb40
olawale <olawaleisaac9@gmail.com>
2025-10-30 21:59:59 +0100

    Fixes

--- app/(tabs-user)/gallery.tsx
+++ app/(tabs-user)/gallery.tsx
@@ -8,7 +8,6 @@ const GalleryScreen = () => {
 
 	const { user } = useAppSelector((state) => state.auth);
 
-	console.log(user.userId, "dd");
 	useEffect(() => {
 		if (user.userId && !user.faceIdPhotoUrl) {
 			setTimeout(() => {
--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,64 @@
-import React from "react";
-import { ActivityIndicator, ImageBackground, View } from "react-native";
+import authService from "@/redux/auth/authService";
+import { getUserInfo } from "@/redux/auth/authSlice";
+import { displayError } from "@/utils/error";
+import { useAppDispatch, useAppSelector } from "@/utils/hooks";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import {
+	ActivityIndicator,
+	ImageBackground,
+	Text,
+	TouchableOpacity,
+	View,
+} from "react-native";
 
 const LoginScreen = () => {
+	const dispatch = useAppDispatch();
+
+	const router = useRouter();
+
+	const params = useLocalSearchParams();
+
+	const { user } = useAppSelector((state) => state.auth);
+
+	useEffect(() => {
+		loginWithMagicLink();
+	}, [params]);
+
+	const loginWithMagicLink = async () => {
+		if (params?.magic_token) {
+			try {
+				let res = await authService.magicLinkLogin(params.magic_token);
+				if (res?.token) {
+					await AsyncStorage.setItem("@accesstoken", res.token);
+				}
+				let user = res?.user;
+				if (user?.userId) {
+					dispatch(getUserInfo());
+				}
+				if (res.changePassword) {
+					router.replace({
+						pathname: "/(app)/changepassword",
+						params: { new: "true" },
+					});
+				} else if (user?.userId && !user.faceIdPhotoUrl) {
+					router.push({
+						pathname: "/(auth)/faceverify",
+						params: user,
+					});
+				} else if (user?.userId) {
+					router.replace("/(tabs-user)/gallery");
+				} else {
+					router.replace("/(auth)/login");
+				}
+			} catch (err) {
+				displayError(err, true);
+				router.replace("/(auth)/login");
+			}
+		}
+	};
+
 	return (
 		<ImageBackground
 			source={require("../assets/images/hairsap_home.png")}
@@ -15,7 +72,13 @@ const LoginScreen = () => {
 					alignItems: "center",
 				}}
 			>
-				<ActivityIndicator color={"#fff"} size={"large"} />
+				{user?.userId ? (
+					<TouchableOpacity>
+						<Text>Access</Text>
+					</TouchableOpacity>
+				) : (
+					<ActivityIndicator color={"#fff"} size={"large"} />
+				)}
 			</View>
 		</ImageBackground>
 	);
